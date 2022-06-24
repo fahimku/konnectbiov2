69920d9d958519031b1ed17b1e46e5600c049a06
kbfahim <kbfahim@konnect.bio>
2022-06-24 19:50:10 +0500

    Sub Category

--- src/pages/linkinbio/component/ShopRightBar/subCategory.js
+++ src/pages/linkinbio/component/ShopRightBar/subCategory.js
@@ -7,121 +7,112 @@ var updateCategory = "";
 var Categories = [];
 let IdData = "";
 function SubCategories({
-    getSubCategories,
-    subcategories,
-    subcategoryId,
-    categoryData
+  getSubCategories,
+  subcategories,
+  subcategoryId,
+  categoryData,
 }) {
-    const [loader, setLoader] = useState(true);
-    const [data, setData] = useState('');
-    useEffect(() => {
+  const [loader, setLoader] = useState(true);
+  const [data, setData] = useState("");
 
- 
-if(subcategoryId.category !=''){
-    updateCategory = subcategoryId.category;
-}
-        if (subcategoryId.subCategoryId != '') {
-          
-            // id = subcategoryId.subCategoryId[0]
-            // var subId = id.split(/[, ]+/).pop();
+  console.log(subcategoryId, "Jhellla");
+  useEffect(() => {
+    if (subcategoryId.category != "") {
+      updateCategory = subcategoryId.category;
+    }
+    if (subcategoryId.subCategoryId != "") {
+      // id = subcategoryId.subCategoryId[0]
+      // var subId = id.split(/[, ]+/).pop();
 
-            setLoader(false);
-            getSubCategories(subcategoryId.subCategoryId)
-                .then((res) => {
-                    setLoader(true);
-                })
-                .catch((res) => {
-                });
+      setLoader(false);
+      getSubCategories(subcategoryId.subCategoryId)
+        .then((res) => {
+          setLoader(true);
+        })
+        .catch((res) => {});
+    }
+  }, [subcategoryId]);
+
+  useEffect(() => {
+    if (IdData != "") {
+      GetSubCategory(IdData);
+    }
+  }, [IdData]);
+
+  useEffect(() => {
+    if (subcategoryId.category != "") {
+      subcategoryId.categories.map((item) => {
+        if (item.value == updateCategory) {
+          IdData = item.parentId;
         }
-    }, [subcategoryId]);
-  
-
-useEffect(() =>{
-    if(IdData != ""){
-    GetSubCategory(IdData)
+      });
     }
-},[IdData])
+  }, [subcategoryId.category]);
 
-    useEffect(() => {
-       if(subcategoryId.category !='' ){
-        subcategoryId.categories.map((item)=>{
-            if(item.value == updateCategory){
-             IdData = item.parentId;
-           
-             }
-       })
+  const GetSubCategory = (val) => {
+    if (val != "") {
+      setLoader(false);
+      getSubCategories(val)
+        .then((res) => {
+          console.log(res, "res");
+          setData(subcategoryId.IdSub);
+          setLoader(true);
+        })
+        .catch((res) => {});
     }
+  };
+  const changeCategory = (value) => {
+    setData(value);
+    categoryData(value);
+  };
+  console.log(data, "data");
 
-    }, [subcategoryId.category])
+  return (
+    <div className="select-categories mt-3">
+      {subcategories.length > 0 || subcategoryId.IdSub != "" ? (
+        <>
+          <label>Select Sub Category</label>
 
-    const GetSubCategory = (val) =>{
-        if(val != ""){
-        getSubCategories(val)
-            .then((res) => {
-                setData(IdData)
-                setLoader(true);
-            })
-            .catch((res) => {
-            });
-        }
-
-    }
-    const changeCategory = (value) => {
-        setData(value);
-        categoryData(value);
-
-    };
-    return (
-        <div className="select-categories mt-3">
-
-            {subcategories.length > 0 || subcategoryId.IdSub != ""?
-                <>
-
-                    <label>Select Sub Category</label>
-
-                    <Select
-                        
-                        key={Date.now()}
-                        value={data}
-                        showSearch
-                        style={{ width: "100%" }}
-                        placeholder="Select Sub Category"
-                        optionFilterProp="children"
-                        clearable={false}
-                        searchable={false}
-                        required
-                        onChange={changeCategory}
-                        // onFocus={onFocus}
-                        // onBlur={onBlur}
-                        // onSearch={onSearch}
-                        filterOption={(input, option) =>
-                            option.children
-                                .toLowerCase()
-                                .indexOf(input.toLowerCase()) >= 0
-                        }
-                    // disabled={
-                    //     PermissionHelper.categoryCheck() || props.singleLoading
-                    //         ? true
-                    //         : false
-                    // }
-                    >
-                        {subcategories.map(({ value, label }, i) => (
-                            <Option value={value}>{label}</Option>
-                        ))}
-                    </Select>
-                </>
-                :
-                <></>
+          <Select
+            key={Date.now()}
+            value={subcategoryId.IdSub == "" ? null : data}
+            showSearch
+            style={{ width: "100%" }}
+            placeholder="Select Sub Category"
+            optionFilterProp="children"
+            clearable={false}
+            searchable={false}
+            required
+            onChange={changeCategory}
+            // onFocus={onFocus}
+            // onBlur={onBlur}
+            // onSearch={onSearch}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
             }
-        </div>
-    )
+            // disabled={
+            //     PermissionHelper.categoryCheck() || props.singleLoading
+            //         ? true
+            //         : false
+            // }
+          >
+            {subcategories.map(({ value, label }, i) => (
+              <Option value={value}>{label}</Option>
+            ))}
+          </Select>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
 function mapStateToProps({ getSubCategories, subcategories }) {
-    return {
-        getSubCategories, subcategories
-
-    };
+  return {
+    getSubCategories,
+    subcategories,
+  };
 }
 
 export default connect(mapStateToProps, { ...sub })(SubCategories);
